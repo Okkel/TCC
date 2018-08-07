082eb1f9308eddcb8ae6b950746e5f1d8e5740b5
arthu <arthur.rodrigues96@gmail.com>
2018-08-07 11:15:30 -0300

    preprocessamento pronto, porem nao automatizado

--- criador_de_sementes.py
+++ criador_de_sementes.py
@@ -4,32 +4,52 @@ import copy
 from igraph import *
 import time
 import numpy as np
+import pickle
+import networkx as nx
+
 
 if sys.argv[2] == 'd':
 	g = Graph.Read_Ncol(sys.argv[1], directed = True)
 elif sys.argv[2] == 'n':
 	g = Graph.Read_Ncol(sys.argv[1], directed = False)
 
-n = 600
+A = g.get_edgelist()
+nxg = nx.Graph(A)
+all_lines = []
 my_list = []
-print (g.vcount())
 print('grau')
-a = g.degree(g.vs(), mode=ALL, loops=False)
-my_list.append(a[:n])
+a = nx.degree_centrality(nxg).values()
+my_list.append(a)
 
-print('betweenness')
-a = g.betweenness()
-my_list.append(a[:n])
+print('betweennes')
+a = nx.betweenness_centrality(nxg).values() 
+my_list.append(a)
 
 print('pagerank')
-a = g.pagerank(g.vs())
-my_list.append(a[:n])
+a = nx.pagerank(nxg).values()
+my_list.append(a)
 
 print('closeness')
-a = g.closeness()
-my_list.append(a[:n])
+a = nx.closeness_centrality(nxg).values()
+my_list.append(a)
 
+print('eigenvector_centrality')
+a = nx.eigenvector_centrality(nxg).values()
+my_list.append(a)
 
+print('harmonic_centrality')
+a = nx.harmonic_centrality(nxg).values()
+my_list.append(a)
+
+print('load_centrality')
+a = nx.load_centrality(nxg).values()
+my_list.append(a)
+
+for i in my_list:
+    print (len(i))
+
+with open('centralidades-'+str(sys.argv[1])+'picke.txt', 'wb') as f:
+    pickle.dump(my_list, f)
 with open('sementes.txt', 'w') as f:
     for s in my_list:
         f.write(str(s))
